25b54ed11bc4345477a004446afb9f7057720c1a
Shub4am <shub4am@gmail.com>
2025-03-26 13:26:49 +0530

    Init Mentor Out of service

--- src/components/ServicesCard.tsx
+++ src/components/ServicesCard.tsx
@@ -9,6 +9,7 @@ import {
 } from "react-icons/md";
 import { RiShoppingBag4Line } from "react-icons/ri";
 import Tabs from "./Tabs";
+import Image from "next/image";
 
 const tabs = [
   { label: "All" },
@@ -21,6 +22,7 @@ const tabs = [
 
 function ServicesCard() {
   const [activeTab, setActiveTab] = useState("All");
+  const [isServicesDisabled, setIsServicesDisabled] = useState(false);
 
   const services = [
     {
@@ -81,99 +83,134 @@ function ServicesCard() {
 
   return (
     <div>
-      <Tabs
-        tabs={tabs}
-        activeTab={activeTab}
-        onTabChange={(label) => setActiveTab(label)}
-      />
-      <div className="grid grid-cols-2 gap-8 pb-20 pt-5">
-        {filteredServices.map((service, index) => (
-          <div
-            key={index}
-            className={`drop-shadow-2xl ${
-              service.heading === "SQL Basics Cheat Sheet"
-                ? "w-full h-[528px] rounded-xl row-span-3"
-                : "w-full h-[248px] rounded-xl "
-            } border border-[#CBD5E1] p-6 bg-white `}
-          >
-            {service.heading === "SQL Basics Cheat Sheet" && (
-              <div className=" bg-[#05192D] rounded-sm p-4 mt-2 h-64 flex flex-col gap-2 justify-center items-center mb-4">
-                <h1 className="text-2xl text-[#03EF62]">
-                  Associate Data engineer in SQl
-                </h1>
-                <p className="text-lg text-white">
-                  Gain practical knowledge in ETL, SQl and data warehousing for
-                  data engineering
-                </p>
-              </div>
-            )}
-            <div className="flex justify-between items-start h-14">
-              <h1 className="text-xl text-[#334155]">{service.heading}</h1>
-              <button className="bg-[#334155] p-2 text-white rounded-lg font-medium text-xs">
-                View Details
-              </button>
-            </div>
-            <p className="py-2 text-sm font-normal leading-5 text-[#334155] h-10">
-              {service.subheading.split("\n").map((line, index) => (
-                <React.Fragment key={index}>
-                  {line}
-                  <br />
-                </React.Fragment>
-              ))}
-            </p>
-
-            <div className="flex w-full items-center p-2 py-4 gap-4">
+      {!isServicesDisabled && (
+        <>
+          <Tabs
+            tabs={tabs}
+            activeTab={activeTab}
+            onTabChange={(label) => setActiveTab(label)}
+          />
+          <div className="grid grid-cols-2 gap-8 pb-20 pt-5">
+            {filteredServices.map((service, index) => (
               <div
-                className={`bg-[#EEF2FF] rounded-sm p-4 flex-1 px-2 ${
+                key={index}
+                className={`drop-shadow-2xl ${
                   service.heading === "SQL Basics Cheat Sheet"
-                    ? "basis-1/2"
-                    : ""
-                }`}
+                    ? "w-full h-[528px] rounded-xl row-span-3"
+                    : "w-full h-[248px] rounded-xl "
+                } border border-[#CBD5E1] p-6 bg-white `}
               >
-                <p className="text-[#64748B] text-sm pb-2">Service type</p>
-                <div className="flex gap-2 items-center">
-                  {service.icon}
-                  <p className="text-sm whitespace-nowrap">
-                    {service.service_type}
-                  </p>
+                {service.heading === "SQL Basics Cheat Sheet" && (
+                  <div className=" bg-[#05192D] rounded-sm p-4 mt-2 h-64 flex flex-col gap-2 justify-center items-center mb-4">
+                    <h1 className="text-2xl text-[#03EF62]">
+                      Associate Data engineer in SQl
+                    </h1>
+                    <p className="text-lg text-white">
+                      Gain practical knowledge in ETL, SQl and data warehousing
+                      for data engineering
+                    </p>
+                  </div>
+                )}
+                <div className="flex justify-between items-start h-14">
+                  <h1 className="text-xl text-[#334155]">{service.heading}</h1>
+                  <button
+                    className={`p-2 rounded-lg font-medium text-xs ${
+                      isServicesDisabled
+                        ? "bg-gray-400 cursor-not-allowed"
+                        : "bg-[#334155] text-white hover:bg-[#1F2937]"
+                    }`}
+                    disabled={isServicesDisabled}
+                  >
+                    View Details
+                  </button>
                 </div>
-              </div>
+                <p className="py-2 text-sm font-normal leading-5 text-[#334155] h-10">
+                  {service.subheading.split("\n").map((line, index) => (
+                    <React.Fragment key={index}>
+                      {line}
+                      <br />
+                    </React.Fragment>
+                  ))}
+                </p>
 
-              {service.heading !== "SQL Basics Cheat Sheet" && (
-                <div className="w-[2px] h-16 bg-[#E2E8F0]"></div>
-              )}
+                <div className="flex w-full items-center p-2 py-4 gap-4">
+                  <div
+                    className={`bg-[#EEF2FF] rounded-sm p-4 flex-1 px-2 ${
+                      service.heading === "SQL Basics Cheat Sheet"
+                        ? "basis-1/2"
+                        : ""
+                    }`}
+                  >
+                    <p className="text-[#64748B] text-sm pb-2">Service type</p>
+                    <div className="flex gap-2 items-center">
+                      {service.icon}
+                      <p className="text-sm whitespace-nowrap">
+                        {service.service_type}
+                      </p>
+                    </div>
+                  </div>
 
-              {service.heading !== "SQL Basics Cheat Sheet" ? (
-                <div className="bg-[#EEF2FF] rounded-sm p-4 flex-1 px-2">
-                  <p className="text-[#64748B] text-sm pb-2">
-                    {service.second_category}
-                  </p>
-                  <div className="flex gap-2 items-center">
-                    <GoClock size={20} className="text-[#3B82F6]" />
-                    <p className="text-sm">{service.duration}</p>
+                  {service.heading !== "SQL Basics Cheat Sheet" && (
+                    <div className="w-[2px] h-16 bg-[#E2E8F0]"></div>
+                  )}
+
+                  {service.heading !== "SQL Basics Cheat Sheet" ? (
+                    <div className="bg-[#EEF2FF] rounded-sm p-4 flex-1 px-2">
+                      <p className="text-[#64748B] text-sm pb-2">
+                        {service.second_category}
+                      </p>
+                      <div className="flex gap-2 items-center">
+                        <GoClock size={20} className="text-[#3B82F6]" />
+                        <p className="text-sm">{service.duration}</p>
+                      </div>
+                    </div>
+                  ) : null}
+
+                  <div className="w-[2px] h-16 bg-[#E2E8F0]"></div>
+
+                  <div
+                    className={`bg-[#EEF2FF] rounded-sm p-4 flex-1 px-2 ${
+                      service.heading === "SQL Basics Cheat Sheet"
+                        ? "basis-1/2"
+                        : ""
+                    }`}
+                  >
+                    <p className="text-[#64748B] text-sm pb-2">Amount</p>
+                    <div className="flex items-center">
+                      <MdCurrencyRupee size={16} className="text-[#00C16A]" />
+                      <p className="text-sm">{service.amount}</p>
+                    </div>
                   </div>
                 </div>
-              ) : null}
-
-              <div className="w-[2px] h-16 bg-[#E2E8F0]"></div>
-
-              <div
-                className={`bg-[#EEF2FF] rounded-sm p-4 flex-1 px-2 ${
-                  service.heading === "SQL Basics Cheat Sheet"
-                    ? "basis-1/2"
-                    : ""
-                }`}
-              >
-                <p className="text-[#64748B] text-sm pb-2">Amount</p>
-                <div className="flex items-center">
-                  <MdCurrencyRupee size={16} className="text-[#00C16A]" />
-                  <p className="text-sm">{service.amount}</p>
-                </div>
               </div>
-            </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </>
+      )}
+
+      {isServicesDisabled && (
+        <div className="mt-4 p-6 bg-white">
+          <div className="flex flex-col items-center gap-4">
+            <div className="flex flex-col justify-center items-center py-6 gap-5">
+              <Image
+                src="/error.png"
+                alt="error"
+                width={0}
+                height={0}
+                className="w-20 h-20"
+                quality={100}
+                unoptimized
+              />
+              <h2 className="text-2xl text-[#334155] font-semibold leading-8 text-center">
+                Temporarily out of <br /> service
+              </h2>
+            </div>
+            <button className="bg-[#334155] text-white px-6 py-2 rounded-lg text-xs font-medium">
+              Find other mentors
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
